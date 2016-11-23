710eea6e69647d280ce86660e221b08806a42c75
Chad Yates <cyates@dynfxdigital.com>
2016-11-22 23:35:34 -0800

    Change color and print cue variables.

--- XactTests/XactTests/Game1.cs
+++ XactTests/XactTests/Game1.cs
@@ -1,27 +1,28 @@
-using System;
-using System.Collections.Generic;
+// Copyright© 2016-2016 Chad C. Yates (cyates@dynfxdigital.com)
+
+#region Using
+
+using System.Diagnostics.Contracts;
 using System.IO;
-using System.Linq;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
+
+#endregion
 
 namespace XactTests
 {
-  /// <summary>
-  /// This is the main type for your game
-  /// </summary>
-  public class Game1 : Microsoft.Xna.Framework.Game
+  /// <summary>This is the main type for your game</summary>
+  public class Game1 : Game
   {
-    GraphicsDeviceManager graphics;
-    AudioEngine audio;
-    SpriteBatch spriteBatch;
-    private WaveBank waveBank;
-    private SoundBank soundBank;
+    private GraphicsDeviceManager graphics;
+    private readonly AudioEngine audio;
+    private readonly WaveBank waveBank;
+    private readonly SoundBank soundBank;
+
+    private SpriteFont debugFont;
+    private SpriteBatch spriteBatch;
 
     private KeyboardState keyboardState;
     private KeyboardState previousKeyboardState;
@@ -32,16 +33,15 @@ namespace XactTests
 
       graphics = new GraphicsDeviceManager(this);
 
-      audio = new AudioEngine(Path.Combine(Content.RootDirectory ,"XactTests.xgs"));
+      audio = new AudioEngine(Path.Combine(Content.RootDirectory, "XactTests.xgs"));
       waveBank = new WaveBank(audio, Path.Combine(Content.RootDirectory, "Wave Bank 1.xwb"));
       soundBank = new SoundBank(audio, Path.Combine(Content.RootDirectory, "Sound Bank 1.xsb"));
     }
 
     /// <summary>
-    /// Allows the game to perform any initialization it needs to before starting to run.
-    /// This is where it can query for any required services and load any non-graphic
-    /// related content.  Calling base.Initialize will enumerate through any components
-    /// and initialize them as well.
+    ///   Allows the game to perform any initialization it needs to before starting to run. This is where it can query for any
+    ///   required services and load any non-graphic related content.  Calling base.Initialize will enumerate through any
+    ///   components and initialize them as well.
     /// </summary>
     protected override void Initialize()
     {
@@ -51,20 +51,19 @@ namespace XactTests
     }
 
     /// <summary>
-    /// LoadContent will be called once per game and is the place to load
-    /// all of your content.
+    ///   LoadContent will be called once per game and is the place to load all of your content.
     /// </summary>
     protected override void LoadContent()
     {
       // Create a new SpriteBatch, which can be used to draw textures.
       spriteBatch = new SpriteBatch(GraphicsDevice);
+      debugFont = Content.Load<SpriteFont>("DebugFont");
 
       // TODO: use this.Content to load your game content here
     }
 
     /// <summary>
-    /// UnloadContent will be called once per game and is the place to unload
-    /// all content.
+    ///   UnloadContent will be called once per game and is the place to unload all content.
     /// </summary>
     protected override void UnloadContent()
     {
@@ -84,21 +83,20 @@ namespace XactTests
     }
 
     /// <summary>
-    /// Allows the game to run logic such as updating the world,
-    /// checking for collisions, gathering input, and playing audio.
+    ///   Allows the game to run logic such as updating the world, checking for collisions, gathering input, and playing audio.
     /// </summary>
     /// <param name="gameTime">Provides a snapshot of timing values.</param>
     protected override void Update(GameTime gameTime)
     {
       // Allows the game to exit
       if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
-        this.Exit();
+        Exit();
 
       // Handle input.
       {
         keyboardState = Keyboard.GetState();
 
-        var startKey = Keys.S;
+        Keys startKey = Keys.S;
         if (cue == null)
         {
           if (IsKeyPressed(startKey))
@@ -130,15 +128,61 @@ namespace XactTests
       base.Update(gameTime);
     }
 
-    /// <summary>
-    /// This is called when the game should draw itself.
-    /// </summary>
+    /// <summary>This is called when the game should draw itself.</summary>
     /// <param name="gameTime">Provides a snapshot of timing values.</param>
     protected override void Draw(GameTime gameTime)
     {
-      GraphicsDevice.Clear(Color.CornflowerBlue);
+      float numCueInstances = 0;
+      float attackTime = 0;
+      float releaseTime = 0;
+      float dopplerPitchScalar = 0;
+      float orientationAngle = 0;
+      float distance = 0;
+
+      Color clearColor;
+      if (cue != null)
+      {
+        numCueInstances = cue.GetVariable("NumCueInstances");
+        attackTime = cue.GetVariable("AttackTime");
+        releaseTime = cue.GetVariable("ReleaseTime");
+        dopplerPitchScalar = cue.GetVariable("DopplerPitchScalar");
+        orientationAngle = cue.GetVariable("OrientationAngle");
+        distance = cue.GetVariable("Distance");
+
+        cue.SetVariable("DopplerPitchScalar", 2);
+
+        if (cue.IsPlaying)
+        {
+          clearColor = Color.Gray;
+        }
+        else if (cue.IsStopping)
+        {
+          clearColor = Color.LightGray;
+        }
+        else
+        {
+          clearColor = Color.Blue;
+        }
+      }
+      else
+      {
+        clearColor = Color.Black;
+      }
+
+      GraphicsDevice.Clear(clearColor);
 
       // TODO: Add your drawing code here
+
+      spriteBatch.Begin();
+
+      spriteBatch.DrawString(debugFont, string.Format("NumCueInstances={0}", numCueInstances), new Vector2(0, 0), Color.White);
+      spriteBatch.DrawString(debugFont, string.Format("AttackTime={0}", attackTime), new Vector2(0, 10), Color.White);
+      spriteBatch.DrawString(debugFont, string.Format("ReleaseTime={0}", releaseTime), new Vector2(0, 20), Color.White);
+      spriteBatch.DrawString(debugFont, string.Format("DopplerPitchScalar={0}", dopplerPitchScalar), new Vector2(0, 30), Color.White);
+      spriteBatch.DrawString(debugFont, string.Format("OrientationAngle={0}", orientationAngle), new Vector2(0, 40), Color.White);
+      spriteBatch.DrawString(debugFont, string.Format("Distance={0}", distance), new Vector2(0, 50), Color.White);
+
+      spriteBatch.End();
 
       base.Draw(gameTime);
     }
